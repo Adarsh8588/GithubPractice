afd612157fc6464e90cbc7c731b124c0ba8d7340
adars <adars@ADARSH>
2023-01-26 19:17:05 +0530

    ok

--- src/test/java/GithubPackage/fg.java
+++ src/test/java/GithubPackage/fg.java
@@ -2,8 +2,9 @@ package GithubPackage;
 
 public class fg {
 
-	public static void main(String[] args) {
-System.out.println("bye");
+	public static void main(String[] args) 
+	{
+     System.out.println("bye");
 	}
 
 }
